abae6826fd3d17222267c34f02395f9bb0c2fdac
eballo <enric.ballo@gmail.com>
2017-11-23 00:09:09 +0100

    add more questions

--- js/questions.js
+++ js/questions.js
@@ -1,15 +1,15 @@
 var questions = [
             {
-            "question" : "A quina comarca pertany la poblacio de Sant Boi de Llobreagt ?",
+            "question" : "De quin periode de temps parlem quan diem edat mitjana ?",
             "image" : "",
             "choices" : [
-                                    "Barcelones",
-                                    "Baix Llobregat",
-                                    "Baix Emporda",
-                                    "Osona"
+                                    "segles V – XII",
+                                    "segles II - VI",
+                                    "segles V - XV",
+                                    "segles VI - X"
                                 ],
-            "correct" : 2,
-            "explanation" : "Sant Boi de Llobregat és una vila i municipi de la comarca del Baix Llobregat. Des de començaments del segle XXI la població supera els 80.000 habitants.",
+            "correct" : 3,
+            "explanation" : "Wikipedia: L’edat mitjana o edat medieval és el període intermedi de la història d'Europa enmig de l'edat antiga i l'edat moderna. Els seus inicis es marquen per dos grans esdeveniments: l'inici al segle V amb la caiguda de l'Imperi Romà, l'any 476, i el final al segle XV amb la caiguda de Constantinoble, el 1453, o bé amb el descobriment d'Amèrica l'any 1492. El nom va ser posat pels humanistes del Renaixement com a terme despectiu, perquè la consideraven una època fosca compresa enmig de moments d'esplendor cultural.",
         },
         {
             "question" : "A quina comarca es troba el castell de Balsareny ?",
@@ -23,4 +23,100 @@ var questions = [
             "correct" : 1,
             "explanation" : "El castell de Balsareny és una fortalesa d'estil gòtic civil català ubicada dalt d'un turó, sobre el municipi de Balsareny.Balsareny és un poble, cap del municipi del mateix nom, situat a la vall del Llobregat, al nord de la comarca del Bages",
         },
+        {
+            "question" : "A quina comarca es troba el castell de Balsareny ?",
+            "image" : "http://www.catalunyamedieval.es/wp-content/gallery/castell-de-balsareny/castell-de-balsareny-080927_502.jpg",
+            "choices" : [
+                                    "Bages",
+                                    "Baix Llobregat",
+                                    "Tarragona",
+                                    "Barcelona"
+                                ],
+            "correct" : 1,
+            "explanation" : "El castell de Balsareny és una fortalesa d'estil gòtic civil català ubicada dalt d'un turó, sobre el municipi de Balsareny.Balsareny és un poble, cap del municipi del mateix nom, situat a la vall del Llobregat, al nord de la comarca del Bages",
+        },
+        {
+            "question" : "A quina comarca es troba el castell de Balsareny ?",
+            "image" : "http://www.catalunyamedieval.es/wp-content/gallery/castell-de-balsareny/castell-de-balsareny-080927_502.jpg",
+            "choices" : [
+                                    "Bages",
+                                    "Baix Llobregat",
+                                    "Tarragona",
+                                    "Barcelona"
+                                ],
+            "correct" : 1,
+            "explanation" : "El castell de Balsareny és una fortalesa d'estil gòtic civil català ubicada dalt d'un turó, sobre el municipi de Balsareny.Balsareny és un poble, cap del municipi del mateix nom, situat a la vall del Llobregat, al nord de la comarca del Bages",
+        },
+        {
+            "question" : "A quina comarca es troba el castell de Balsareny ?",
+            "image" : "http://www.catalunyamedieval.es/wp-content/gallery/castell-de-balsareny/castell-de-balsareny-080927_502.jpg",
+            "choices" : [
+                                    "Bages",
+                                    "Baix Llobregat",
+                                    "Tarragona",
+                                    "Barcelona"
+                                ],
+            "correct" : 1,
+            "explanation" : "El castell de Balsareny és una fortalesa d'estil gòtic civil català ubicada dalt d'un turó, sobre el municipi de Balsareny.Balsareny és un poble, cap del municipi del mateix nom, situat a la vall del Llobregat, al nord de la comarca del Bages",
+        },
+        {
+            "question" : "A quina comarca es troba el castell de Balsareny ?",
+            "image" : "http://www.catalunyamedieval.es/wp-content/gallery/castell-de-balsareny/castell-de-balsareny-080927_502.jpg",
+            "choices" : [
+                                    "Bages",
+                                    "Baix Llobregat",
+                                    "Tarragona",
+                                    "Barcelona"
+                                ],
+            "correct" : 1,
+            "explanation" : "El castell de Balsareny és una fortalesa d'estil gòtic civil català ubicada dalt d'un turó, sobre el municipi de Balsareny.Balsareny és un poble, cap del municipi del mateix nom, situat a la vall del Llobregat, al nord de la comarca del Bages",
+        },
+        {
+            "question" : "A quina comarca es troba el castell de Balsareny ?",
+            "image" : "http://www.catalunyamedieval.es/wp-content/gallery/castell-de-balsareny/castell-de-balsareny-080927_502.jpg",
+            "choices" : [
+                                    "Bages",
+                                    "Baix Llobregat",
+                                    "Tarragona",
+                                    "Barcelona"
+                                ],
+            "correct" : 1,
+            "explanation" : "El castell de Balsareny és una fortalesa d'estil gòtic civil català ubicada dalt d'un turó, sobre el municipi de Balsareny.Balsareny és un poble, cap del municipi del mateix nom, situat a la vall del Llobregat, al nord de la comarca del Bages",
+        },
+        {
+            "question" : "A quina comarca es troba el castell de Balsareny ?",
+            "image" : "http://www.catalunyamedieval.es/wp-content/gallery/castell-de-balsareny/castell-de-balsareny-080927_502.jpg",
+            "choices" : [
+                                    "Bages",
+                                    "Baix Llobregat",
+                                    "Tarragona",
+                                    "Barcelona"
+                                ],
+            "correct" : 1,
+            "explanation" : "El castell de Balsareny és una fortalesa d'estil gòtic civil català ubicada dalt d'un turó, sobre el municipi de Balsareny.Balsareny és un poble, cap del municipi del mateix nom, situat a la vall del Llobregat, al nord de la comarca del Bages",
+        },
+        {
+            "question" : "A quina comarca es troba el castell de Balsareny ?",
+            "image" : "http://www.catalunyamedieval.es/wp-content/gallery/castell-de-balsareny/castell-de-balsareny-080927_502.jpg",
+            "choices" : [
+                                    "Bages",
+                                    "Baix Llobregat",
+                                    "Tarragona",
+                                    "Barcelona"
+                                ],
+            "correct" : 1,
+            "explanation" : "El castell de Balsareny és una fortalesa d'estil gòtic civil català ubicada dalt d'un turó, sobre el municipi de Balsareny.Balsareny és un poble, cap del municipi del mateix nom, situat a la vall del Llobregat, al nord de la comarca del Bages",
+        },
+        {
+            "question" : "A quina comarca es troba el castell de Balsareny ?",
+            "image" : "http://www.catalunyamedieval.es/wp-content/gallery/castell-de-balsareny/castell-de-balsareny-080927_502.jpg",
+            "choices" : [
+                                    "Bages",
+                                    "Baix Llobregat",
+                                    "Tarragona",
+                                    "Barcelona"
+                                ],
+            "correct" : 1,
+            "explanation" : "El castell de Balsareny és una fortalesa d'estil gòtic civil català ubicada dalt d'un turó, sobre el municipi de Balsareny.Balsareny és un poble, cap del municipi del mateix nom, situat a la vall del Llobregat, al nord de la comarca del Bages",
+        }
 ];
