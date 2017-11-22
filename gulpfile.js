505934fe0bb5d01fe2d0e033d7731425449a101c
eballo <enric.ballo@gmail.com>
2017-11-23 00:26:40 +0100

    refactor - assets directory + add sound file

--- gulpfile.js
+++ gulpfile.js
@@ -9,10 +9,16 @@ var onError = function(err) {
 
 // minify-js
 gulp.task('minify-js', function() {
-  gulp.src('./node_modules/js-cookie/src/js.cookie.js') // path to your files
+  gulp.src(['./node_modules/js-cookie/src/js.cookie.js','./src/js/*.js']) // path to your files
     .pipe(uglify())
-    .pipe(gulp.dest('./js/'));
+    .pipe(gulp.dest('./assets/js/'));
+});
+
+// copy css
+gulp.task('copy-css', function () {
+    gulp.src('./src/css/*')
+        .pipe(gulp.dest('./assets/css/'));
 });
 
 // Lets us type "gulp" on the command line and run all of our tasks
-gulp.task('default', ['minify-js']);
+gulp.task('default', ['minify-js','copy-css']);
